2bb8a428edce39bec78890b05c811340535725c5
hongshan <275280587@qq.com>
2020-11-19 17:24:35 +0800

    init

---
+++ src/components/form.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <a-form :from="form">
+      <a-form-item v-for="item in formList" :key="item">
+        <a-input v-decorator="[ item ]" />
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'form-com',
+  data() {
+    return {
+      form: this.$form.createForm(this, { name: 'form' }),
+      formList: []
+    };
+  },
+  methods: {
+    initForm() {
+      const element = [];
+      for (let index = 1; index < 100; index++) {
+        element.push((1000 + index).toString())
+      }
+      this.formList.splice(0, this.formList.length, ...element)
+    }
+  },
+  mounted() {
+    this.initForm()
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/table.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <a-table
+      :columns="columns"
+      :data-source="tableList"
+      :pagination="false"
+      row-key="ID"
+    ></a-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'table-com',
+  data() {
+    return {
+      columns: [
+        { title: "ID", dataIndex: "ID", key: "ID" },
+        { title: "姓名", dataIndex: "name", key: "name" },
+      ],
+      tableList: []
+    };
+  },
+  methods: {
+    initTableData() {
+      const element = [];
+      for (let index = 1; index < 1000; index++) {
+        let obj = {
+          ID: 1000 + index,
+          name: '郭文龙'
+        }
+        element.push(obj)
+      }
+      this.tableList.splice(0, this.tableList.length, ...element)
+    }
+  },
+  mounted() {
+    this.initTableData()
+  }
+}
+</script>
+<style scoped>
+
+</style>
