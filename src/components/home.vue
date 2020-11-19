a89755acdf429d7fd7c9638091402de2ace3d8d6
hongshan <275280587@qq.com>
2020-11-19 16:47:07 +0800

    init

---
+++ src/components/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>首页</div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {};
+  },
+  methods: {},
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <a-button @click="current = 'home'">首页</a-button>
+    <a-button @click="current = 'table'">表格</a-button>
+    <a-button @click="current = 'form'">表单</a-button>
+    <home-com v-if="current === 'home'"></home-com>
+    <table-com v-if="current === 'table'"></table-com>
+    <form-com v-if="current === 'form'"></form-com>
+  </div>
+</template>
+
+<script>
+import HomeCom from './home'
+import TableCom from './table'
+import FormCom from './form'
+export default {
+  name: 'index',
+  components: {
+    HomeCom,
+    TableCom,
+    FormCom
+  },
+  data() {
+    return {
+      current: 'home'
+    };
+  },
+  methods: {},
+}
+</script>
+<style scoped>
+
+</style>
